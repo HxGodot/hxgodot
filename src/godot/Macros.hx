f8d25ab96297268a51d971a4c58ee4983530fe65
Michael Bickel <mib@developium.net>
2022-07-30 14:21:43 +0200

    refactor: postinit for engine and extension class

--- src/godot/Macros.hx
+++ src/godot/Macros.hx
@@ -34,11 +34,50 @@ class Macros {
         //trace(className);
         //trace(parent_class_name);
 
+        // add necessary metadata to the class
+        classMeta.add(":headerCode", [macro "#include <godot/gdnative_interface.h>"], pos);
+        classMeta.add(":headerClassCode", [macro "
+                static void *___binding_create_callback(void *p_token, void *p_instance) {                                     
+                    return nullptr;                                                                                            
+                }                                                                                                              
+                static void ___binding_free_callback(void *p_token, void *p_instance, void *p_binding) {                       
+                }                                                                                                              
+                static GDNativeBool ___binding_reference_callback(void *p_token, void *p_instance, GDNativeBool p_reference) { 
+                    return true;                                                                                               
+                }                                                                                                              
+                static constexpr GDNativeInstanceBindingCallbacks ___binding_callbacks = {                                     
+                    ___binding_create_callback,                                                                                
+                    ___binding_free_callback,                                                                                  
+                    ___binding_reference_callback,
+                };
+            "], pos);
+
         // helper function
         function addPostInitMethod(_class_name:String) {
             var postInitClass = macro class {
+                static var __class_name = $v{className};
+                static var __parent_class_name = $v{parent_class_name};
+
                 override function __postInit() {
                     __owner = godot.Types.GodotNativeInterface.classdb_construct_object($v{_class_name});
+
+                    var ptr:cpp.RawPointer<cpp.Void> = untyped __cpp__('(void*){0}.mPtr', this);
+                    
+                    if (!$v{isEngineClass}) { // deadcode elimination will get rid of this
+                        godot.Types.GodotNativeInterface.object_set_instance(
+                            __owner, 
+                            __class_name, 
+                            cast ptr
+                        );    
+                    }
+                    
+                    // register the callbacks, do we need this?
+                    godot.Types.GodotNativeInterface.object_set_instance_binding(
+                        __owner, 
+                        untyped __cpp__("godot::internal::token"), 
+                        cast ptr, 
+                        untyped __cpp__('&___binding_callbacks')
+                    );
                 }
             }
             fields = fields.concat(postInitClass.fields);
@@ -51,24 +90,6 @@ class Macros {
         else {
             // add postinit, adds the owner
             addPostInitMethod(parent_class_name);
-
-            // add necessary metadata to the class
-            classMeta.add(":headerCode", [macro "#include <godot/gdnative_interface.h>"], pos);
-            classMeta.add(":headerClassCode", [macro "
-                    static void *___binding_create_callback(void *p_token, void *p_instance) {                                     
-                        return nullptr;                                                                                            
-                    }                                                                                                              
-                    static void ___binding_free_callback(void *p_token, void *p_instance, void *p_binding) {                       
-                    }                                                                                                              
-                    static GDNativeBool ___binding_reference_callback(void *p_token, void *p_instance, GDNativeBool p_reference) { 
-                        return true;                                                                                               
-                    }                                                                                                              
-                    static constexpr GDNativeInstanceBindingCallbacks ___binding_callbacks = {                                     
-                        ___binding_create_callback,                                                                                
-                        ___binding_free_callback,                                                                                  
-                        ___binding_reference_callback,
-                    };
-                "], pos);
 
             // find the first engine-class up the inheritance chain
             var engine_parent = cls.get().superClass.t.get();
@@ -84,29 +105,12 @@ class Macros {
 
             // add all necessary fields for godot interop
             var regClass = macro class {
-                static var __class_name = $v{className};
-                static var __parent_class_name = $v{parent_class_name};
-                
                 public static function __create(_data:godot.Types.VoidPtr):godot.Types.GDNativeObjectPtr { 
                     var n = new $typePath();
                     
                     // make sure hx GC keeps us around as long as godot has an owner for us
                     trace("GCAddRoot");
                     untyped __cpp__("GCAddRoot((hx::Object **)&{0}.mPtr)", n);
-                    var ptr:cpp.RawPointer<cpp.Void> = untyped __cpp__('(void*){0}.mPtr', n);
-                    godot.Types.GodotNativeInterface.object_set_instance(
-                        n.__owner, 
-                        __class_name, 
-                        cast ptr
-                    );
-                    
-                    // register the callbacks, do we need this?
-                    godot.Types.GodotNativeInterface.object_set_instance_binding(
-                        n.__owner, 
-                        untyped __cpp__("godot::internal::token"), 
-                        cast ptr, 
-                        untyped __cpp__('&___binding_callbacks')
-                    );
                     
                     return n.__owner;
                 }
