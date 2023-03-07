2c9d9e5433cd937b7b0627b2e4aa90388fa3d2e3
Michael Bickel <mib@developium.net>
2023-03-07 01:27:06 +0100

    fix dynamic pointer

--- src/godot/macros/Macros.hx
+++ src/godot/macros/Macros.hx
@@ -989,7 +989,7 @@ class Macros {
             }
 
             static function __registerMethods() {
-                var library = cpp.Pointer.fromStar(untyped __cpp__("godot::internal::library"));
+                var library:cpp.Pointer<cpp.Void> = cpp.Pointer.fromStar(untyped __cpp__("godot::internal::library"));
                 
                 // register all methods
                 $b{regOut};
